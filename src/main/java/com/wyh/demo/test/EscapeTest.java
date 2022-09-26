db1bb0aab4f260ef01c5ef0dd494ae6b27c2a52f
imaiOshima <616357796@qq.com>
2022-09-26 13:40:12 +0800

    LeetCode改代码 jvm的逃逸分析

--- src/main/java/com/wyh/demo/test/EscapeTest.java
+++ src/main/java/com/wyh/demo/test/EscapeTest.java
@@ -1,51 +1,28 @@
 package com.wyh.demo.test;
 
-import com.sun.org.apache.bcel.internal.generic.RETURN;
-
+import java.sql.Time;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @Classname EscapeTest
- * @Description TODO
- * @Date 2022/9/20 0:33
- * @Created by 61635
+ * @author imai
+ * @since 2022/9/20 9:25 上午
  */
 public class EscapeTest {
     public static void main(String[] args) throws InterruptedException {
-        for(int i = 0;i<=1000;i++){
-            getAge();
+        long start = System.currentTimeMillis();
+        for(int i = 0;i<1000000;i++){
+            alloc();
         }
+        long end = System.currentTimeMillis();
+        System.out.printf("时间消耗"+ (end - start));
         TimeUnit.HOURS.sleep(1);
     }
 
-    public static int getAge(){
-        Studetn studetn = new Studetn("小明",18);
-        return studetn.getAge();
+    public static void alloc(){
+        User user = new User();
     }
+}
 
-    static class Studetn{
-        private String name;
-        private int age;
+class User{
 
-        public Studetn(String name, int age) {
-            this.name = name;
-            this.age = age;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
-    }
 }
